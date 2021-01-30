4140a1b5940a680d01ec0601d3d45b89587b4922
Görkem Karagöl <gorkem.karagol@ogr.sakarya.edu.tr>
2021-01-30 15:24:44 +0300

    30 Ocak

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class CustomerManager
+    {
+        //İş Sınıflar-business classes
+        public void Ekle(Musteri musteri)
+        {
+           
+
+
+
+        }
+    }
+}
